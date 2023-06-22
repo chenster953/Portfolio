a854cc4514defe30a3656ed255f5543c6a0b9a43
zchen10 <zchen10@wgu.edu>
2023-06-22 13:59:07 -0700

    :)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import pfp from './pfp-woman.png'
 
 import './hero.scss'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className="message"></div>
-      <div className="image"></div>
+      <div className="upper">
+      <div className="message">
+        <h2>Front-End React Developer</h2>
+        <p>Hi, I'm James Chen. A passionate Front-end React Developer based in Fremont, California.</p>
+      </div>
+      <div className="image">
+        <img src={pfp} alt="pfp" />
+      </div>
+      </div>
       <div className="techStack"></div>
     </div>
   )
